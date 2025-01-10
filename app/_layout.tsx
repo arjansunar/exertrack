2c67b0eae241fdc91fc784ecc264dd84903f9cda
arjansunar <arjan.gahatrajsunar@gmail.com>
2025-01-10 15:39:15 +0545

    feat: migrate on app load

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,31 @@
 import { Stack } from "expo-router";
 
+import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
 import "../global.css";
+import migrations from "@/db/migrations/migrations";
+import { db } from "@/db";
+import { Text, View } from "react-native";
+
+// TODO:
+// Loading screen to setup db
 export default function RootLayout() {
+  const { success, error } = useMigrations(db, migrations);
+
+  if (error) {
+    return (
+      <View>
+        <Text>Migration error: {error.message}</Text>
+      </View>
+    );
+  }
+
+  if (!success) {
+    return (
+      <View>
+        <Text>Migration is in progress...</Text>
+      </View>
+    );
+  }
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
