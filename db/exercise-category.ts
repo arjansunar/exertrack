43ac5227e3728ea45014d30d9f728dabd2c92e1c
arjansunar <arjan.gahatrajsunar@gmail.com>
2025-01-10 16:11:47 +0545

    feat: add await keyword

--- db/exercise-category.ts
+++ db/exercise-category.ts
@@ -3,11 +3,11 @@ import { db } from ".";
 import { exerciseCategories } from "./schema";
 
 export async function create(category: typeof exerciseCategories.$inferInsert) {
-  return db.insert(exerciseCategories).values(category);
+  return await db.insert(exerciseCategories).values(category);
 }
 
 export async function update(id: number, name: string) {
-  return db
+  return await db
     .update(exerciseCategories)
     .set({
       name,
@@ -16,9 +16,11 @@ export async function update(id: number, name: string) {
 }
 
 export async function remove(id: number) {
-  return db.delete(exerciseCategories).where(eq(exerciseCategories.id, id));
+  return await db
+    .delete(exerciseCategories)
+    .where(eq(exerciseCategories.id, id));
 }
 
 export async function all() {
-  return db.select().from(exerciseCategories);
+  return await db.select().from(exerciseCategories);
 }
