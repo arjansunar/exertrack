f4c63083c258f1969ac522933c2b3a50b2854433
arjansunar <arjan.gahatrajsunar@gmail.com>
2025-01-10 15:25:54 +0545

    feat: add drizzle

---
+++ db/schema.ts
@@ -0,0 +1,23 @@
+import { relations } from "drizzle-orm";
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+
+// Define tables
+
+export const exerciseCategories = sqliteTable("exerciseCategories", {
+  id: integer("id").primaryKey(),
+  name: text("name"),
+});
+
+export const exercises = sqliteTable("exercises", {
+  id: integer("id").primaryKey(),
+  name: text("name").notNull(),
+  categoryId: integer("category_id"),
+  description: text("description"),
+});
+
+export const exerciseRelations = relations(exercises, ({ one }) => ({
+  category: one(exerciseCategories, {
+    fields: [exercises.categoryId],
+    references: [exerciseCategories.id],
+  }),
+}));
